59792d3aa6bcd8871a48de972f868d167fa46da9
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-06-17 06:51:28 -0500

    Added Grunt and CSS compilation options

---
+++ wp-content/themes/aslan2014/Gruntfile.js
@@ -0,0 +1,117 @@
+'use strict';
+module.exports = function(grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger sass, jshint, uglify and livereload
+        watch: {
+            sass: {
+                files: ['assets/styles/*.{scss,sass}'],
+                tasks: ['sass', 'autoprefixer', 'cssmin']
+            },
+            js: {
+                files: '<%= jshint.all %>',
+                tasks: ['jshint', 'uglify']
+            },
+            images: {
+                files: ['assets/images/**/*.{png,jpg,gif}'],
+                tasks: ['imagemin']
+            },
+            livereload: {
+                options: { livereload: true },
+                files: ['style.css', 'assets/js/*.js', 'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}']
+            }
+        },
+
+        // sass
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: false,
+                    style: 'expanded',
+                },
+                files: {
+                    'assets/styles/build/style.css': 'assets/styles/style.scss',
+                    'assets/styles/build/editor-style.css': 'assets/styles/editor-style.scss'
+                }
+            }
+        },
+
+        // autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9', 'ios 6', 'android 4'],
+                map: true
+            },
+            files: {
+                expand: true,
+                flatten: true,
+                src: 'assets/styles/build/*.css',
+                dest: 'assets/styles/build'
+            },
+        },
+
+        // css minify
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'assets/styles/build',
+                src: ['*.css', '!*.min.css'],
+                ext: '.css'
+            }
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                "force": true
+            },
+            all: [
+                'Gruntfile.js',
+                'assets/js/*.js'
+            ]
+        },
+
+        // uglify to concat, minify, and make source maps
+        uglify: {
+            main: {
+                options: {
+                    sourceMap: 'assets/js/build/main.js.map',
+                    sourceMappingURL: 'main.js.map',
+                    sourceMapPrefix: 2
+                },
+                files: {
+                    'assets/js/build/main.min.js': [
+                        'assets/js/main.js'
+                    ]
+                }
+            }
+        },
+
+        // image optimization
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true,
+                    interlaced: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'assets/images/'
+                }]
+            }
+        }
+
+    });
+
+    // register task
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'uglify', 'imagemin', 'watch']);
+
+};
