bd15c09d5e63a586198c857c8da1740f16d0762c
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-08-02 20:23:34 -0500

    Added initial footer menu & related styles

--- wp-content/themes/aslan2014/footer.php
+++ wp-content/themes/aslan2014/footer.php
@@ -1,3 +1,8 @@
+		<footer class="site-footer">
+			<nav class="nav-footer">
+				<a href="/wp-admin">My Account</a>
+			</nav>
+		</footer>
 	</div><!-- /.site-wrapper -->
 	<?php wp_footer(); ?>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/scripts/build/main.min.js"></script>
