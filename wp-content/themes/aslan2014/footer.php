8640af76698592a039d632c5e90e27ce2a0ef2c0
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-08-05 20:52:38 -0500

    Added modal

--- wp-content/themes/aslan2014/footer.php
+++ wp-content/themes/aslan2014/footer.php
@@ -4,6 +4,10 @@
 			</nav>
 		</footer>
 	</div><!-- /.site-wrapper -->
+	<div class="modal-overlay hide"></div>
+	<div class="modal-window hide">
+		<a href="#" class="modal-close">X</a>
+	</div>
 	<?php wp_footer(); ?>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/scripts/build/main.min.js"></script>
 </body>
