1af0e5cca7b1d7837db140bb74c41b50470edb40
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-08-13 21:11:30 -0500

    Moved a bunch of providers stuff to include

--- wp-content/themes/aslan2014/functions.php
+++ wp-content/themes/aslan2014/functions.php
@@ -152,78 +152,7 @@ function aslaninst_tabs_reorder_columns($columns) {
 
 function aslaninst2014_init(){
 
-	/**
-	 * PROVIDERS
-	 *
-	 */
-
-	add_post_type_support( 'page', 'excerpt' );
-
-	//post type
-    register_post_type( 'aslaninst_provider',
-        array(
-            'labels' => array(
-                'name'          => __( 'Providers' ),
-                'singular_name' => __( 'Provider' ),
-                'add_new_item'  => __( 'Add Provider' ),
-                'edit_item'     => __( 'Update Provider' )
-            ),
-            'description'           => 'Providers for listing page',
-            'public'                => true,
-            'exclude_from_search'   => false,
-            'supports'              => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
-            'menu_icon'				=> 'dashicons-nametag',
-            'rewrite'				=> array('slug' => 'provider')
-        )
-    );
-
-    //taxonomy for insurances
-	$labels = array(
-		'name'              => _x( 'Insurances', 'taxonomy general name' ),
-		'singular_name'     => _x( 'Insurance', 'taxonomy singular name' ),
-		'search_items'      => __( 'Search Insurances Accepted' ),
-		'all_items'         => __( 'All Insurances' ),
-		'edit_item'         => __( 'Edit Insurance' ),
-		'update_item'       => __( 'Update Insurance' ),
-		'add_new_item'      => __( 'Add New Insurance' ),
-		'new_item_name'     => __( 'New Insurance' ),
-		'menu_name'         => __( 'Insurance Options' ),
-	);
-
-	$args = array(
-		'hierarchical'      => false,
-		'labels'            => $labels,
-		'show_ui'           => true,
-		'show_admin_column' => false,
-		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'insurances' ),
-	);
-
-	register_taxonomy( 'aslaninst_insurance', array( 'aslaninst_provider' ), $args );
-
-    //and a taxonomy for specialties
-	$labels = array(
-		'name'              => _x( 'Specialties', 'taxonomy general name' ),
-		'singular_name'     => _x( 'Specialty', 'taxonomy singular name' ),
-		'search_items'      => __( 'Search Specialties' ),
-		'all_items'         => __( 'All Specialties' ),
-		'edit_item'         => __( 'Edit Specialty' ),
-		'update_item'       => __( 'Update Specialty' ),
-		'add_new_item'      => __( 'Add New Specialty' ),
-		'new_item_name'     => __( 'New Specialty' ),
-		'menu_name'         => __( 'Specialties' ),
-	);
-
-	$args = array(
-		'hierarchical'      => false,
-		'labels'            => $labels,
-		'show_ui'           => true,
-		'show_admin_column' => false,
-		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'specialties' ),
-	);
-
-	register_taxonomy( 'aslaninst_specialty', array( 'aslaninst_provider' ), $args );
+	providers_init();
 
 	/**
 	 * TRAINING
@@ -305,24 +234,6 @@ function aslaninst2014_enqueue_scripts(){
 }
 function aslaninst2014_admin_queue_scripts( $hook ){
 	wp_enqueue_style( 'aslaninst2014-style', get_template_directory_uri() . '/admin.css', array() );
-}
-
-function aslaninst2014_providers_list( $id, $member_type ){
-
-	$args = array(
-			'post_type' => 'aslaninst_provider',
-			'post_status' => 'publish',
-			'meta_key' => 'member_type',
-			'meta_value' => $member_type
-		);
-	$providers = get_posts( $args );
-
-	$return = '';
-	foreach( $providers as $provider ):
-		$return .= include(locate_template('snippets/provider-listing-entry.php'));
-	endforeach;
-	return $return;
-
 }
 
 function aslaninst2014_splash_animation(){
