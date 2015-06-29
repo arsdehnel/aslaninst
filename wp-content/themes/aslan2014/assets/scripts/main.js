5e14eb4faeb03cc11ef16f71c32c9b2760b899ff
Adam Dehnel (home) <arsdehnel@gmail.com>
2015-06-28 22:42:35 -0500

    initial responsive styling

--- wp-content/themes/aslan2014/assets/scripts/main.js
+++ wp-content/themes/aslan2014/assets/scripts/main.js
@@ -61,32 +61,8 @@ var aslanInstitute = {
 
 	readMore: function($targetObj) {
 
-		var $el, $ps, $up, totalHeight;
-
-		totalHeight = 0;
-
-		$el = $targetObj;
-		$p  = $el.parent();
-		$up = $p.parent();
-		$ps = $up.find("p:not('.read-more')");
-
-		// measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
-		$ps.each(function() {
-			totalHeight += jQuery(this).outerHeight();
-		});
-
-		$up
-			.css({
-			  // Set height to prevent instant jumpdown when max height is removed
-			  "height": $up.height(),
-			  "max-height": 9999
-			})
-			.animate({
-			  "height": totalHeight
-			});
-
-		// fade out read-more
-		$p.fadeOut();
+		$targetObj.closest('article').css('height','auto');
+		$targetObj.fadeOut();
 
 	}
 
