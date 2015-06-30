d2069506698c5610effd691f7bb9146fce94f448
Adam Dehnel (home) <arsdehnel@gmail.com>
2015-06-29 22:00:57 -0500

    initial responsive styling

--- wp-content/themes/aslan2014/partials/provider-selector.php
+++ wp-content/themes/aslan2014/partials/provider-selector.php
@@ -1,5 +1,21 @@
 <div class="provider-select-wrapper">
-	<a href="#" class="provider-select-toggle">view provider list</a>
+	<a href="#" class="provider-select-toggle">
+		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+			<g>
+				<rect x="55" y="234" width="56" height="45" rx="20" ry="20"></rect>
+				<rect x="156" y="234" width="299" height="45" rx="20" ry="20"></rect>
+			</g>
+			<g>
+				<rect x="156" y="334" width="299" height="45" rx="20" ry="20"></rect>
+				<rect x="55" y="334" width="56" height="45" rx="20" ry="20"></rect>
+			</g>
+			<g>
+				<rect x="156" y="434" width="299" height="45" rx="20" ry="20"></rect>
+				<rect x="55" y="434" width="56" height="45" rx="20" ry="20"></rect>
+			</g>
+		</svg>
+		view provider list
+	</a>
 	<p>Leadership Team</p>
 	<nav class="provider-select leadership">
 		<?php
