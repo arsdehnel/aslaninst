058c5f1233e55bc1ad3c59116b11520ef038050c
Adam Dehnel (home) <arsdehnel@gmail.com>
2015-06-30 22:08:42 -0500

    initial responsive styling

--- wp-content/themes/aslan2014/partials/content-header.php
+++ wp-content/themes/aslan2014/partials/content-header.php
@@ -2,13 +2,13 @@
 	<h2><?php the_title(); ?></h2>
 	<p><?php the_excerpt(); ?></p>
     <?php
-        if( is_user_logged_in() ):
+        // if( is_user_logged_in() ):
             if( get_field('registration_type') == "ecommerce" ):
                 $shortcode = '[add_to_cart_url id="'.get_field('registration_product')->ID.'"]';
                 echo '<a class="register-now-button" href="'.do_shortcode($shortcode).'">Register Now</a>';
             // elseif( get_field('registration_type') == "email" ):
                 // echo 'email registration_type';
             endif;
-        endif;
+        // endif;
     ?>
 </header>
