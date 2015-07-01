1e0938e7bd1ed788fca0dfb309e1df79fc02cf69
Adam Dehnel (home) <arsdehnel@gmail.com>
2015-06-30 21:43:07 -0500

    initial responsive styling

--- wp-content/themes/aslan2014/templates/provider-listing.php
+++ wp-content/themes/aslan2014/templates/provider-listing.php
@@ -13,6 +13,23 @@ get_header();
 			<h2><?php the_title(); ?></h2>
 			<p><?php the_excerpt(); ?></p>
 	</header>
+	<a href="#" class="provider-select-toggle">
+		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+			<g>
+				<rect x="55" y="0" width="56" height="45" rx="20" ry="20"></rect>
+				<rect x="156" y="0" width="299" height="45" rx="20" ry="20"></rect>
+			</g>
+			<g>
+				<rect x="156" y="100" width="299" height="45" rx="20" ry="20"></rect>
+				<rect x="55" y="100" width="56" height="45" rx="20" ry="20"></rect>
+			</g>
+			<g>
+				<rect x="156" y="200" width="299" height="45" rx="20" ry="20"></rect>
+				<rect x="55" y="200" width="56" height="45" rx="20" ry="20"></rect>
+			</g>
+		</svg>
+		view provider list
+	</a>
 	<div class="provider-select-wrapper">
 		<p>Leadership Team</p>
 		<nav class="provider-select leadership">
